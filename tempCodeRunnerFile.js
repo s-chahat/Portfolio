5d01842a0e3e46ba3e0bad1f7e088879cfeef4eb
s-chahat <s.chahat.2357@gmail.com>
2025-10-04 16:24:44 +0530

    first commit

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,9 @@
+const video1 = document.getElementById('projectVideo1');
+const video2 = document.getElementById('projectVideo2');
+const video3 = document.getElementById('projectVideo3');
+
+// Sidebar elements //
+const sideBar = document.querySelector('.sidebar');
+const menu = document.querySelector('.menu-icon');
+const closeIcon = document.querySelector('.close-icon')
+
